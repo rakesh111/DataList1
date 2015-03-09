523b88a04dd41319458b38290ee057e2ef4627f7
rakesh111 <rakesh1.mohan@gmail.com>
2015-03-09 16:27:05 +0530

    Created new solution. Added ItemList. Initialized Parse Client

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Parse;
+
+namespace SampleList.Droid
+{
+	[Activity(Label = "DataList", MainLauncher = true, Icon = "@drawable/icon")]
+	public class HomeScreen : ListActivity {
+		string[] items;
+		protected async override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			ParseClient.Initialize("6jg1ny9edOT5Ezjvyj6zGDw5wnxvWsITjid1wrgy","ohSxCU4RsFrReBUe3AJeL9lRhFD5EHKJl1tNvgMc");
+
+
+			{
+				var itemList = new ParseObject ("ItemList");
+				itemList ["ItemName"] = "ball";
+				await itemList.SaveAsync ();
+			}
+			items = new string[] { "Item1","Item2","Item3","Item4","Item6","Item7" };
+			ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+		}
+		protected override void OnListItemClick(ListView l, View v, int position, long id)
+		{
+			var t = items[position];
+			Android.Widget.Toast.MakeText(this, t, Android.Widget.ToastLength.Short).Show();
+		}
+	}
+}
+
+	
+
---
+++ Droid/obj/Debug/android/src/samplelist/droid/HomeScreen.java
@@ -0,0 +1,55 @@
+package samplelist.droid;
+
+
+public class HomeScreen
+	extends android.app.ListActivity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onListItemClick:(Landroid/widget/ListView;Landroid/view/View;IJ)V:GetOnListItemClick_Landroid_widget_ListView_Landroid_view_View_IJHandler\n" +
+			"";
+		mono.android.Runtime.register ("SampleList.Droid.HomeScreen, SampleList.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", HomeScreen.class, __md_methods);
+	}
+
+
+	public HomeScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == HomeScreen.class)
+			mono.android.TypeManager.Activate ("SampleList.Droid.HomeScreen, SampleList.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3)
+	{
+		n_onListItemClick (p0, p1, p2, p3);
+	}
+
+	private native void n_onListItemClick (android.widget.ListView p0, android.view.View p1, int p2, long p3);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
